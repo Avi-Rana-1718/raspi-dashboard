9fa5405fb074bfbcdf597228ac34d1677f7ffc23
Avi Rana <itzavi1718@gmail.com>
2024-12-05 20:32:32 +0530

    added more info

--- routes/systemRoutes.js
+++ routes/systemRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express")
 const router = express.Router();
 const os = require("os");
+var os_utils = require('os-utils');
+let diskspace = require("diskspace");
 
 router.get("/info", (req, res)=>{
     let sysObj = {
         platform: os.platform(),
         architecture: os.arch(),
-        freemem: Math.floor(os.freemem()/Math.pow(1024, 3)),
-        totalmem: Math.floor(os.totalmem()/Math.pow(1024,3)),
-        processor: os.cpus()[0]
+        freemem: os.freemem()/Math.pow(1024, 3),
+        totalmem: os.totalmem()/Math.pow(1024,3),
+        processor: {model: os.cpus()[0].model.trim(),
+            cores:  os_utils.cpuCount()
+        },
+        diskspace: diskspace.check('C', (err, res)=>{return res})
     }
     res.json(sysObj)
 })
